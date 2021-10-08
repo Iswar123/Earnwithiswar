15b02367a2a4c656adf4dcca188c8f83ab713a7f
Iswar123 <92151072+Iswar123@users.noreply.github.com>
2021-10-08 14:39:37 +0530

    Update register.php

--- register.php
+++ register.php
@@ -22,7 +22,7 @@
                 $result = mysqli_query($conn, $sql);
 
                 if ($result) {
-                    header("Location: index.php");
+                    header("Location: index.html");
                 }else {
                     echo "<script>Error: ".$sql.mysqli_error($conn)."</script>";
                 }
